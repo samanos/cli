672517bda0a155a143f06b6112cf97ef6a62346d
Martynas Mickevičius <mmartynas@gmail.com>
2015-05-09 22:51:45 +0300

    Use renamed usb-interface methods.

--- src/cli.c
+++ src/cli.c
@@ -12,7 +12,7 @@ int main() {
   };
 
   uint8_t reply[32];
-  int8_t length = transfer_command1(&device_id, 0xA1 + (0xBE << 8), reply, sizeof(reply));
+  int8_t length = transfer_data2(&device_id, REQUEST_BRIDGE_STATUS, 0, reply, sizeof(reply));
   if (length >= 0) {
     printf("Reply has size: %02X\n", length);
     printf("First reply bytes: %02X %02X %02X %02X\n", reply[0], reply[1], reply[2], reply[3]);
